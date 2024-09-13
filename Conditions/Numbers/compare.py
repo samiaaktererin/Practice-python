422f26ffded9d643626be1c296e0562a635f4864
samiaaktererin <142722098+samiaaktererin@users.noreply.github.com>
2024-09-13 15:49:52 +0600

    Compare.py

---
+++ Conditions/Numbers/compare.py
@@ -0,0 +1,50 @@
+x = int(input("Whats x? ")) 
+y = int(input("Whats y? "))
+
+#if statement
+
+# if x < y:
+#     print("x is less than y")
+# if x > y:
+#     print("x is greater than y")
+# if x == y:
+#     print("x is equal to y")
+
+#if elif statement
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x is equal to y")
+
+#if elif else statement
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+#using or
+
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+# using !=
+
+# if x != y: 
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+# using ==
+
+if x == y: 
+    print("x is  equal to y")
+else:
+    print("x is not equal to y")
